fcb08735828cf148cd9fbc45f0f56f615a715fb9
hjx <huangjiaxin2@yy.com>
2018-12-13 16:58:52 +0800

    Keep the same as MosChat code.

--- LuaKitProject/AndroidDemo/app/src/main/java/com/common/luakit/utils/TextViewUtils.java
+++ LuaKitProject/AndroidDemo/app/src/main/java/com/common/luakit/utils/TextViewUtils.java
@@ -4,11 +4,11 @@ public class TextViewUtils {
 
     private static final String TAG = "TextViewUtils";
 
-    public static float calculatorHeightOfTextView(String text,float textWidth, float textSize, String fontName) {
+    public static float calculatorHeightOfTextView(String text, float textWidth, float textSize, String fontName) {
         return 0f;
     }
 
-    public static float calculatorWidthOfTextView(String text,float textHeight, float textSize, String fontName) {
+    public static float calculatorWidthOfTextView(String text, float textHeight, float textSize, String fontName) {
         return 0f;
     }
 
